29809806f216143ca1f33d68cab2eda7caeb3161
applet <celbever@naver.com>
2023-01-24 00:43:56 +0900

    [main] config.js 오류수정

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB,
+    database: process.env.DB,
     host: process.env.HOST,
     dialect: "mysql"
   },
